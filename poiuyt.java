ddb3895664287c6ee8dcf927cbf059c5010329ed
liams333 <liamkstewart8@gmail.com>
2014-11-13 17:59:48 -0800

    Update poiuyt.java
    I have just added the first path (the path after the first 3 questions) The path is only updated for answer 1 and 3 of the Original Question.

--- poiuyt.java
+++ poiuyt.java
@@ -40,30 +40,66 @@ public class Main {
 		String text;
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-	
-		System.out.println("You have just woken up, school starts in 30 minutes! \n What would you like to do? \n 1) Go back to bed. \n 2) Get dressed. \n 3) Lay in bed for 5 minutes.");
-		//Question1
+		
+		//Original Question FINISHED!
+		System.out.println("You have just woken up, school starts in 30 minutes! \nWhat would you like to do? \n 1) Go back to bed. \n 2) Get dressed. \n 3) Lay in bed for 5 minutes.");
+		
 		text = scan.nextLine();
-	//1
-		if(text.equals("1")){
-				System.out.println(one);
-		}
+			//1
+				if(text.equals("1")){
+		
+					if(position==1){
+					System.out.println(one + "\n" + position + "/26" + "\n*****************************");
+					}
+				}
 			//2
 				if(text.equals("2")){
 					
 					position ++;
-					if(position==1);
-					System.out.println(two);
+					if(position==2){
+					System.out.println(two + "\n" + position + "/26" + "\n*****************************");
+					}
 				}
-					//3
-						if(text.equals("3")){
-							System.out.println(one);
-						}
-		if (text=="1"){
-			System.out.println("You have just woken up, school starts in 30 minutes! \n What would you like to do? \n 1) Go back to bed. \n 2) Get dressed. \n 3) Lay in bed for 5 minutes.");	
+			//3
+				if(text.equals("3")){
+					
+					if(position==1){
+					System.out.println(one + "\n" + position + "/26" + "\n*****************************");
+					}
+				}
+	
+	
+		//Path 1-1 FINISHED!
+		if(position==1){
+			System.out.println("The bells is about to go! You slept in too long! \n 1) Skip class, go back to bed. \n 2) Hurry, put clothes on, exit house to school \n 3) Take your time, make a lunch, watch some TV and hopefully make it to school before 2nd block starts.");
+			text = scan.nextLine();
+			if(text.equals("1")){
+				position --;
+				if(position==0){
+					System.out.println(one + " You have chosen to skip class!" + "\n" + position + "/26" + "\n*****************************" );
+				}
+			}
+				if(text.equals("2")){
+				position ++;
+				if(position==2){
+					System.out.println(two + " You rushed out the door" + "\n" + position + "/26" + "\n*****************************");
+				}
+			}
+				if(text.equals("3")){
+				
+				if(position==1){
+					System.out.println(one + " You have chosen nutrition over education!" + "\n" + position + "/26" + "\n*****************************");
 		}
-		
-		
-		}
+				
+			
+		//Path 2-1 NOT FINISHED!
+	
+	
+	
+	
 	
 	}
+}
+	}	
+}
+	
